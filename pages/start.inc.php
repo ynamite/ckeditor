ca280bf9fc051326b7971c38a5b4f77c20ed52a5
RexDude <rexdude@arcor.de>
2014-01-23 18:59:43 +0100

    Logo update

--- pages/start.inc.php
+++ pages/start.inc.php
@@ -1,7 +1,7 @@
 <div class="rex-addon-output">
 	<h2 class="rex-hl2"><?php echo $I18N->msg('ckeditor_start_title'); ?></h2>
 	<div class="rex-area-content">
-		<p id="logo"><a target="_blank" href="http://ckeditor.com"><img src="../<?php echo $REX['MEDIA_ADDON_DIR']; ?>/ckeditor/logo.png" width="320" height="116" alt="" /></a></p>
+		<p id="logo"><a target="_blank" href="http://ckeditor.com"><img src="../<?php echo $REX['MEDIA_ADDON_DIR']; ?>/ckeditor/logo.png" width="340" height="116" alt="" /></a></p>
 		<br />
 		<ul>
 			<li><a class="extern" target="_blank" href="http://ckeditor.com/demo"><?php echo $I18N->msg('ckeditor_start_demo'); ?></a></li>
