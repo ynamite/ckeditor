79f0dc0754416336ab486553b87ad99a5f7218fa
RexDude <rexdude@arcor.de>
2015-08-11 08:32:09 +0200

    Added headline for tools page

--- pages/help/tools.inc.php
+++ pages/help/tools.inc.php
@@ -1,3 +1,5 @@
+<h1><?php echo $I18N->msg('ckeditor_help_chapter_tools'); ?></h1>
+
 <h2><?php echo $I18N->msg('ckeditor_toolbar_configurator'); ?></h2>
 <a target="_blank" href="<?php echo $REX['HTDOCS_PATH'] . 'files/addons/ckeditor/vendor/samples/toolbarconfigurator/index.html'; ?>"><?php echo $I18N->msg('ckeditor_open_toolbar_configurator'); ?></a></li>
 
