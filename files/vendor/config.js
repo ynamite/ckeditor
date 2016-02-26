13945a89f20bae305ea989ecefb0010c9fa71068
RexDude <rexdude@arcor.de>
2016-02-26 22:39:26 +0100

    Added ckeditor version 4.5.7

--- files/vendor/config.js
+++ files/vendor/config.js
@@ -1,29 +1,38 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
-	// For the complete reference:
+	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	config.fillEmptyBlocks = false;
-	config.forcePasteAsPlainText = true;
-	config.entities = false;
-	config.linkShowTargetTab = false;
-	config.format_tags = 'p;h2;h3';
-	config.removePlugins = 'elementspath';
-	config.extraPlugins = 'rex_help';
-	config.removeDialogTabs = 'link:advanced';
-	config.toolbar = [
-		['Format'],
-		['Bold', 'Italic'],
-		['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'],
-		['Link', 'Unlink', 'Anchor'],
-		['Table'],
-		['PasteText', 'PasteFromWord'],
-		['rex_help']
-		// no comma after last entry!!!
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
 	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
